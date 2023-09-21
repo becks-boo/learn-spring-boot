ebae8cb10b3de8ebcaa6b343418146eec37cb06e
Igor Stein <70313147+becks-boo@users.noreply.github.com>
2023-09-21 11:56:47 +0200

    Created authentication for login only with requestparam for now

--- src/main/java/com/learnprojects/learnspringboot/login/LoginController.java
+++ src/main/java/com/learnprojects/learnspringboot/login/LoginController.java
@@ -8,14 +8,29 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoginController {
-    private Logger logger = LoggerFactory.getLogger(getClass());
+    private Logger logger = LoggerFactory.getLogger(getClass());;
+    private AuthenticationService authenticationService;
+
+    public LoginController(AuthenticationService authenticationService) {
+        super();
+        this.authenticationService = authenticationService;
+    }
 
     @RequestMapping("login")
-    public String goToLogin(@RequestParam String name) {
+    public String goToLogin(@RequestParam String name, @RequestParam String password) {
 
-        logger.debug(name);
-        logger.info("I want this printed at info level");
-        logger.warn("I want this printed at warn level");
-        return "Welcome to the page!";
+        if (authenticationService.authenticate(name, password)) {
+            logger.debug(name);
+            logger.info("I want this printed at info level");
+            logger.warn("I want this printed at warn level");
+            return "Welcome to the page!";
+        }
+
+        return "Password not valid";
     }
+
+/*    @RequestMapping(value="login", method = RequestMethod.GET)
+    public String goToLogin() {
+        return "Welcome to the page!";
+    }*/
 }
